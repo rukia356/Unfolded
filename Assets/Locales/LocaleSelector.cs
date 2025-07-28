4e2f00bf8017c1416d1c2f94583b2bb1d46cfe43
kia <kiahuhtamaki@outlook.com>
2025-07-28 18:03:34 +0300

    Change language from database functioning
    can swap between english and finnish for UI

---
+++ Assets/Locales/LocaleSelector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Localization.Settings;
+
+public class LocaleSelector : MonoBehaviour 
+{
+    private bool active = false;
+    public void ChangeLocale(int localeID)
+    {
+        StartCoroutine(SetLocale(localeID));
+    }
+
+    IEnumerator SetLocale(int _localeID)
+    {
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
+        active = false;
+    }
+}
